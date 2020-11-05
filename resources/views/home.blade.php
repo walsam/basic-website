920bb4927e22615c1f042b5b7b22d77052140b1e
Walid Maarad <walsam@hotmail.com>
2020-11-05 17:01:29 +0100

    testing routes, views and layouts

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>home</h1>
+@endsection
+
+@section('sidebar')
+    @parent
+    this is something i added
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Basic Website</title>
+    </head>
+    <body>
+        @yield('content')
+
+        @include('inc.sidebare')
+    </body>
+</html>
